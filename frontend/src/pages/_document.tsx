369f5de479f936d013d92a74348acbb1a39fa437
Jairo Tunisse <jairo_tunisse@outlook.com>
2022-09-14 19:08:46 -0300

    init commit :heavy_check_mark:

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import Document, { Html } from "next/document";
+
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <body>
+
+                </body>
+            </Html>
+
+        )
+    }
+}
